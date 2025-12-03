69f245099200063727a5d64c68c8644df846b5d2
Merrybin <147122885+Sleepingbin@users.noreply.github.com>
2025-12-03 16:05:46 +0800

    Update database password in index.js

--- gpms-backend/db/index.js
+++ gpms-backend/db/index.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "haohaoxuexi999",
+    password: "123456",
     database: "gpms1",
 });
 
